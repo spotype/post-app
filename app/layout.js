5ec03170ed12bed289dc3b12365e9fc54fc2e1d2
Abdul Musabbir <149356080+spotype@users.noreply.github.com>
2024-01-10 09:11:50 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="/logo.svg" type="image/x-icon"></link>
       <body className={inter.className}>{children}</body>
     </html>
   );
